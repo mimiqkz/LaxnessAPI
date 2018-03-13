ec3250fbc8f485ebc532296d99b2ed3d1855f113
zurgur <harryfreyr@gmail.com>
2018-03-13 11:04:44 +0000

    login and out in app.js

--- app.js
+++ app.js
@@ -1,8 +1,103 @@
 require('dotenv').config();
 const express = require('express');
 const api = require('./api');
+const session = require('express-session');
+const passport = require('passport');
+const { Strategy } = require('passport-local');
+const users = require('./db.js');
 
 const app = express();
+const sessionSecret = process.env.SESSION_SECRET || 'fj489jfadkljv';
+
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
+}));
+
+async function strat(username, password, done) {
+  const user = await users.findByUsername(username);
+
+  if (!user) {
+    return done(null, false);
+  }
+
+  let result = false;
+  try {
+    result = await users.comparePasswords(password, user.password);
+  } catch (error) {
+    done(error);
+  }
+
+  if (result) {
+    return done(null, user);
+  }
+
+  return done(null, false);
+}
+
+passport.use(new Strategy(strat));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await users.findById(id);
+    return done(null, user);
+  } catch (error) {
+    return done(error);
+  }
+});
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use((req, res, next) => {
+  if (req.isAuthenticated()) {
+    res.locals.user = req.user.name;
+  }
+
+  res.locals.showLogin = true;
+
+  next();
+});
+
+// hjálparfall fyrir view
+app.locals.isInvalid = (param, errors) => {
+  if (!errors) {
+    return false;
+  }
+
+  return Boolean(errors.find(i => i.param === param));
+};
+
+app.get('/login', (req, res) => {
+  let message = '';
+
+  if (req.session.messages && req.session.messages.length > 0) {
+    message = req.session.messages.join(', ');
+  }
+
+  res.render('login', { showLogin: false, message, title: 'Innskráning' });
+});
+
+app.post(
+  '/login',
+  passport.authenticate('local', {
+    failureMessage: 'Vitlaust notendanafn eða lykilorð',
+    failureRedirect: '/login',
+  }),
+  (req, res) => {
+    res.redirect('/admin');
+  },
+);
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 app.use(express.json());
 app.use('/', api);
